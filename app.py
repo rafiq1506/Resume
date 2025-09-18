0dca7a30a2f08f0703236522df4784cc482bc0e9
rafiq1506 <128342312+rafiq1506@users.noreply.github.com>
2025-09-18 11:04:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template
 from keep_alive import keep_alive
 keep_alive()
 
-app = Flask(__name__)
+# app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='/static')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-
     app.run(debug=True)
+
 
